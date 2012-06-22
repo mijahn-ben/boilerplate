b3e10fd5d3915af97b14b9b309abb8913afa4e6b
Ben Masters <ben@fuel-communications.co.uk>
2012-06-22 07:24:19 +0100

    removed nav li margins

--- header.php
+++ header.php
@@ -25,10 +25,13 @@ Fallback content, in case the browser does not support Canvas.
 </canvas>
 	</a>  
   </div>
+  
+  
+  
 	<nav>
 		<ul class="mainNavigation" role="main navigation">
 			<li><a href="#">Home</a></li>
-			<li><a href="#">Two Column</a></li>
+			<li><a href="#">2 Column</a></li>
 			<li><a href="#">Blog</a></li>
 			<li><a href="#">Contact</a></li>
 			<li><a href="#">3 Column</a></li>
