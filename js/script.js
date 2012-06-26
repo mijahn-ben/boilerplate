b5c9c3f9a591fc40c518324fe5ebbb7b67886271
Mijahn Ben Jameir-Masters <mijahn@mijahn-desktop.(none)>
2012-06-26 17:00:23 +0100

    updated readme

--- js/script.js
+++ js/script.js
@@ -72,5 +72,34 @@ ctx.lineTo(116.5, 27.6);
 ctx.closePath();
 ctx.fill();
 
+// Placeholder for IE
 
+// extend jQuery.support object, create new input object.
 
+jQuery(function() {
+	jQuery.support.placeholder = false;
+	test = document.createElement('input');
+	if('placeholder' in test) jQuery.support.placeholder = true;
+});
+
+// find placeholder attribute, apply values
+
+$(function() {
+	if(!$.support.placeholder) { 
+		var active = document.activeElement;
+		$(':text').focus(function () {
+			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+				$(this).val('').removeClass('hasPlaceholder');
+			}
+		}).blur(function () {
+			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+				$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+			}
+		});
+		$(':text').blur();
+		$(active).focus();
+		$('form').submit(function () {
+			$(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+		});
+	}
+});
