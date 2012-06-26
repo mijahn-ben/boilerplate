5fd887e80e3118b47f623192d8d7f653561329b5
Ben Masters <ben@fuel-communications.co.uk>
2012-06-26 22:25:48 +0100

    added grid 8 and 4 col

--- grid.php
+++ grid.php
@@ -1,69 +1,165 @@
 <?php include('header.php'); ?>  
-  <div id="wrap" role="main">
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
-		</div>
-<!-- end one column -->
-<!--one column -->		
-		<div class="oneGrid">
-			<div class="content">
-				<p>Hello</p>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
-			</div>
+ <div id="wrap" role="main">
+ 
+ <!--one column -->		
+		<div class="oneCol">
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->						
 		</div>
 <!-- end one column -->
 
-  </div>
+ <!--one column -->		
+		<div class="oneCol">
+					<!--Quarter -->		
+						<div class="fourGrid">
+								<div class="content">
+										<p>Quarter</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end Quarter -->
+					<!--Quarter -->		
+						<div class="fourGrid">
+								<div class="content">
+										<p>Quarter</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end Quarter -->	
+					<!--Quarter -->		
+						<div class="fourGrid">
+								<div class="content">
+										<p>Quarter</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end Quarter -->	
+					<!--Quarter -->		
+						<div class="fourGrid">
+								<div class="content">
+										<p>Quarter</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end Quarter -->	
+										
+		</div>
+<!-- end one column -->
+<!--one column -->		
+		<div class="oneCol">
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->
+					<!--Quarter -->		
+						<div class="fourGrid">
+								<div class="content">
+										<p>Quarter</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end Quarter -->
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->
+					<!--Quarter -->		
+						<div class="fourGrid">
+								<div class="content">
+										<p>Quarter</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end Quarter -->	
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->
+					<!--one grid -->		
+						<div class="eightGrid">
+								<div class="content">
+										<p>Eigth</p>
+										<p>Lorem ipsum dolor sit amet, consectetur </p>			
+								</div>
+						</div>
+					<!-- end one grid -->
+										
+		</div>
+<!-- end one column -->
+
+</div>
 <?php include('footer.php'); ?>
