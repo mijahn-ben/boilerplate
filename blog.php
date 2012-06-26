ef49d1a46543156c606a098686719e95edf9663b
Ben Masters <ben@fuel-communications.co.uk>
2012-06-26 07:08:10 +0100

    responsive blog template

---
+++ blog.php
@@ -0,0 +1,36 @@
+<?php include('header.php'); ?>  
+  <div id="wrap" role="main">
+<!--one column -->		
+		<div class="oneCol">
+			<div class="content">
+				<p>Hello</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
+			</div>
+		</div>
+<!-- end one column -->
+
+<!-- single column with  left sidebar -->
+		<div class="mainContent">
+				<div class="content">
+				<p>Hello</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
+				</div>
+				<div class="content">
+				<p>Hello</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
+				</div>
+				<div class="content">
+				<p>Hello</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
+				</div>	
+		</div>
+		<div class="sideBar">
+				<div class="content">
+				<p>Sidebar</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim </p>			
+				</div>
+		</div>
+<!-- end single column with sidebar -->
+
+  </div>
+<?php include('footer.php'); ?>
