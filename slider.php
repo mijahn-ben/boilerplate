448daf88d7f686a59df5c91d20d0c89c1830af5c
Mijahn Ben Jameir-Masters <mijahn@mijahn-desktop.(none)>
2012-06-28 17:26:05 +0100

    css tidy up

--- slider.php
+++ slider.php
@@ -8,10 +8,15 @@
     <ul class="royalSlidesContainer">
     	<!-- Slides -->
         <li class="royalSlide">
-            <img class="royalImage" src="http://placehold.it/650x300&text=Slide+One" width="650" height="300"/>
+            <img class="royalImage" src="http://placehold.it/1300x600&text=Slide+One" width="650" height="300"/>
+            <div class="royalCaption">
+            	
+                <div class="royalCaptionItem" data-show-effect="fade movetop">Caption 1 for image 1</div>
+                
+            </div> 
         </li> 
         <li class="royalSlide">
-            <img class="royalImage" src="http://placehold.it/650x300&text=Slide+Two" width="650" height="300"/>
+            <img class="royalImage" src="http://placehold.it/1300x600&text=Slide+Two" width="650" height="300"/>
             <div class="royalCaption">
             	
                 <div class="royalCaptionItem" data-show-effect="fade movetop">Caption 1 for image 2</div>
@@ -19,7 +24,12 @@
             </div> 
          </li> 
         <li class="royalSlide">
-            <img class="royalImage" src="http://placehold.it/650x300&text=Slide+Three" width="650" height="300"/>
+            <img class="royalImage" src="http://placehold.it/1300x600&text=Slide+Three" width="650" height="300"/>
+            <div class="royalCaption">
+            	
+                <div class="royalCaptionItem" data-show-effect="fade movetop">Caption 1 for image 3</div>
+                
+            </div> 
         </li>
     </ul>
 </div><p>Hello</p>
@@ -27,7 +37,30 @@
 			</div>
 		</div>
 <!-- end one column -->
-
+<!--one column -->		
+		<div class="oneCol clearfix">
+<!-- three column -->
+		<div class="threeCol">
+			<div class="content">
+							<p>Column</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim  </p>	
+			</div>
+		</div>
+		<div class="threeCol">
+			<div class="content">
+							<p>Column</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim  </p>		
+			</div>
+		</div>
+		<div class="threeCol">
+			<div class="content">
+							<p>Column</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam congue leo quis enim  </p>	
+			</div>
+		</div>
+<!-- end three column -->
+		</div>
+<!-- end one column -->
 
 
   </div>
